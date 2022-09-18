c2fc03981b487012b24ddefd22b8e5eb1eac8b81
Ilya <ilyarbox4@gmail.com>
2022-09-18 17:41:46 +0300

    setup emacs flychecker for C

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . (
+	    (flycheck-checker . c/c++-gcc)
+	    (flycheck-gcc-warnings . ("all"))
+	    (mode . Flycheck)
+	    )))
